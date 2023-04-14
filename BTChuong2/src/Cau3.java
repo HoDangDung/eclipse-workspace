8b8f0a9ca74a04050e40ec9a15bfcbe2d10a5f3b
Hồ Đăng Dũng <65653599+HoDangDung@users.noreply.github.com>
2023-04-14 10:49:02 +0700

    done

--- BTChuong2/src/Cau3.java
+++ BTChuong2/src/Cau3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Cau3 {
 
@@ -6,11 +8,47 @@ public class Cau3 {
 
 //		Xác định điểm M nằm trong, trên hay ngoài đường tròn
 		
-//		ĐẦU VÀO:
+// ĐẦU VÀO: Tọa độ (xC, yC) tâm, bán kín đường tròn R, tọa độ điểm (xM, yM)
 		
-//		XỬ LÝ:
+// XỬ LÝ: Tạo biến xC, yC, xM, yM kiểu float, biến M kiểu double, biến R kiểu int
+//		In thông báo nhập xC
+//		Cho người dùng nhập vào giá trị xC và gán vào xC
+//		In thông báo nhập yC
+//		Cho người dùng nhập vào giá trị yC và gán vào yC
+//		In thông báo nhập R
+//		Cho người dùng nhập vào giá trị R và gán vào R
+//		In thông báo nhập xM
+//		Cho người dùng nhập vào giá trị xM và gán vào xM
+//		In thông báo nhập yM
+//		Cho người dùng nhập vào giá trị yM và gán vào yM
 		
-//		ĐẦU RA:
+//		Tính M = R - sqrt((xM-xC)*(xM-xC) + (yM-yC)*(yM-yC)) 
+		
+// ĐẦU RA: Điểm M nằm trên , trong hay ngoài đường tròn
+		
+		Scanner scan = new Scanner(System.in);
+		float xC, yC, xM, yM;
+		double M;
+		int R;
+		
+		System.out.print("Nhập tọa độ xC: ");
+		xC = Float.parseFloat(scan.nextLine());
+		System.out.print("Nhập tọa độ yC: ");
+		yC = Float.parseFloat(scan.nextLine());
+		System.out.print("Nhập tọa độ R: ");
+		R = Integer.parseInt(scan.nextLine());
+		System.out.print("Nhập tọa độ xM: ");
+		xM = Float.parseFloat(scan.nextLine());
+		System.out.print("Nhập tọa độ yM: ");
+		yM = Float.parseFloat(scan.nextLine());
+		
+		M = R - (Math.sqrt((xM-xC)*(xM-xC) + (yM-yC)*(yM-yC)));
+		if (M > 0) {
+			System.out.print("M nằm trong đường tròn");
+		} else if(M < 0){
+			System.out.print("M nằm ngoài đường tròn");
+		}else {
+			System.out.print("M nằm trên đường tròn");
+		}
 	}
-
 }
